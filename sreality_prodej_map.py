da66d4d70e45b044e5e851195934f8d0ee26a1d9
denbuk <62837235+denbuk@users.noreply.github.com>
2023-05-21 23:00:10 +0200

    Update sreality_prodej_map.py

--- sreality_prodej_map.py
+++ sreality_prodej_map.py
@@ -1,1 +1,17 @@
+from collections import namedtuple
+from datetime import datetime as dt
+from PIL import Image
+import altair as alt
+import math
+import pandas as pd
+import numpy as np
+import streamlit as st
+import requests
+import json
+import time
 
+with st.echo(code_location='below'):
+    st.title('Adobe tag generator')
+    with st.spinner('Wait for it...'):
+        time.sleep(2)
+    
