fb98635e82c7152d5d95aee80e75ac4e76e63acc
denbuk <62837235+denbuk@users.noreply.github.com>
2023-05-22 21:19:40 +0200

    update sreality_prodej_map.py

--- sreality_prodej_map.py
+++ sreality_prodej_map.py
@@ -34,7 +34,8 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 # Print results.
 df = pd.DataFrame(rows)
 
-fig = px.scatter_mapbox(df, lat="lat", lon="lon", color="price", color_continuous_scale=px.colors.cyclical.IceFire, hover_name="name", hover_data = ["price"], zoom=6)
+fig = px.scatter_mapbox(df, lat="lat", lon="lon", color="price", color_continuous_scale=[(0, 'blue'), (2000000, 'blue'), (2000000, 'green'), (5000000, 'green'),\
+           (5000000, 'red'), (8000000, 'red'), (8000000, 'yellow'), (20000000, 'yellow')], hover_name="name", hover_data = ["price"], zoom=6)
 
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
