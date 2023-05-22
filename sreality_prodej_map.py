e1689e78e2c4ffddb755fe3a0c71c4f1410774c1
denbuk <62837235+denbuk@users.noreply.github.com>
2023-05-22 20:45:44 +0200

    update sreality_prodej_map.py

--- sreality_prodej_map.py
+++ sreality_prodej_map.py
@@ -15,20 +15,20 @@ conn = connect(credentials=credentials)
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 # @st.cache_data(ttl=600)
-#def run_query(query):
-#    rows = conn.execute(query, headers=1)
-#    rows = rows.fetchall()
-#    return rows
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
 sheet_url = st.secrets["private_gsheets_url"]
-#rows = run_query(f'SELECT * FROM "{sheet_url}"')
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 #sheet_id = "1l5YgElKRpM1trDFaIY-q-rAJ2OaPHLhTBVVQ0Ed0aeQ"
 #sheet_name = "sreality-api-first-prodej"
 #url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-worksheet = conn.worksheet(sheet_url)
-rows = worksheet.get_all_records()
+#worksheet = conn.worksheet(sheet_url)
+#rows = worksheet.get_all_records()
 
 # Print results.
 df = pd.DataFrame(rows)
