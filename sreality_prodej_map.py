ba4dab6d768bb94a4060cb5cc74a8be22a194e50
denbuk <62837235+denbuk@users.noreply.github.com>
2023-05-22 20:32:51 +0200

    update sreality_prodej_map.py

--- sreality_prodej_map.py
+++ sreality_prodej_map.py
@@ -25,7 +25,7 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 sheet_id = "1l5YgElKRpM1trDFaIY"
 sheet_name = "sreality-api-first-prodej"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+url = f'https://docs.google.com/spreadsheets/d/"{sheet_id}"/gviz/tq?tqx=out:csv&sheet="{sheet_name}"'
 
 # Print results.
 df = pd.read_csv(url)
