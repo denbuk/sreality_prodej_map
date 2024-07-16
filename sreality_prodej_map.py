475f49ebfa52eeb624241e446d72a5b6b5507f9d
denbuk <62837235+denbuk@users.noreply.github.com>
2024-07-16 23:12:03 +0200

    update sreality_prodej_map.py

--- sreality_prodej_map.py
+++ sreality_prodej_map.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 import json
 
 # Function to fetch data from API endpoint
@@ -14,37 +13,28 @@ def fetch_data(endpoint):
         return None
 
 # Function to save data to Google Sheets
-def save_to_google_sheets(data, sheet_name):
-    # Define the scope
-    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
+def main():
+    st.title("Fetch Data from API Example")
 
-    # Add credentials to the account
-    creds = ServiceAccountCredentials.from_json_keyfile_name("starry-computer-387421-161a86e54e0a.json", scope)
+    # Input for the API endpoint
+    endpoint = st.text_input("Enter the API endpoint URL:")
 
-    # Authorize the clientsheet 
-    client = gspread.authorize(creds)
+    if st.button("Fetch Data"):
+        if endpoint:
+            data = fetch_data(endpoint)
+            if data:
+                # Display data
+                st.write(data)
 
-    # Get the instance of the Spreadsheet
-    spreadsheet = client.open(sheet_name)
+                # Convert to DataFrame for better display
+                if isinstance(data, list):
+                    df = pd.DataFrame(data)
+                    st.dataframe(df)
+                elif isinstance(data, dict):
+                    df = pd.DataFrame([data])
+                    st.dataframe(df)
+        else:
+            st.warning("Please enter a valid URL.")
 
-    # Create a new sheet with a unique name
-    #new_sheet_name = f"Sheet {len(spreadsheet.worksheets()) + 1}"
-    #worksheet = spreadsheet.add_worksheet(title=new_sheet_name, rows="100", cols="20")
-
-    sr_estates_list = data['_embedded']['estates'][0]
-
-    st.write(sr_estates_list)
-
-# Streamlit app layout
-st.title("Fetch and Save Data")
-''
-
-endpoint = st.text_input("API Endpoint", "https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&ownership=1&per_page=5&tms=1721153792&page=5&floor_number=1%7C1&%26no_auction=1&no_shares=1")
-sheet_name = st.text_input("Google Sheet Name", "sreality-api-update-2")
-
-if st.button("Fetch and Save"):
-    data = fetch_data(endpoint)
-    if data:
-        
-        save_to_google_sheets(data, sheet_name)
-        st.success("Data saved to Google Sheets successfully")
+if __name__ == "__main__":
+    main()
